0379c77b2aa65bfcfeb739ad738fef1c5ea69814
SERGIO <checomperez@gmail.com>
2021-10-16 10:26:35 -0500

    Some settings

--- src/libs/initializer.ts
+++ src/libs/initializer.ts
@@ -1,11 +1,20 @@
+import Action, { ActionModel } from "../models/action-model";
 import Role, { RoleModel } from "../models/role-model";
 
 export default async() => {
     try {
+        // If there are not roles we create the basics :)
         const roles: RoleModel[] = await Role.find();
         if (!roles || roles.length === 0) {
-            await Role.create( { role: 'admin' } );
-            await Role.create( { role: 'user' } );
+            console.log('Initializing roles');
+            await Role.create( { role: 'Administrator' } );
+        }
+
+        // Do the same with actions
+        const actions: ActionModel[] = await Action.find();
+        if ( !actions || actions.length === 0 ) {
+            console.log('Initializing actions');
+            await Action.create( { description: 'admin' } );
         }
     } catch( error ) {
         console.log('No se han podido inicializar los roles');
